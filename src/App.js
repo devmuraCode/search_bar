93fe03698373273731623ffb29c78c6d6436d617
Murod <muradkusherbaev@gmail.com>
2022-03-18 16:39:39 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
 
-function App() {
+const App = () => {
+  const [filter, setFilter] = useState('');
+  const SearchText = (e) => {
+    setFilter(e.target.value)
+  }
+
+  let dataSearch = data.filter(item =>
+    Object.keys(item).some(key =>
+      item[key].toString().toLowerCase().includes(filter.toString().toLowerCase())
+    )
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input type="text" value={filter} onChange={SearchText} />
+      <div className="app">
+        {dataSearch.slice(0, 6).map((el, id) => {
+          return (
+            <div key={id} className="card">
+              <div className="card-img">
+                <img src={el.img} alt="" />
+              </div>
+              <div className="card-body">
+                <p className="card-title">
+                  {el.package}
+                </p>
+                <p className="card-price">
+                  {el.text2}
+                </p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
